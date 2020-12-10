e389944fd7398c9939c27fdf50db6b16b5f92a23
Kelly <kellyahenriksen@gmail.com>
2020-12-10 12:56:42 -0500

    got questions answered

--- PyBank/main.py
+++ PyBank/main.py
@@ -13,32 +13,43 @@ with open(csvpath) as csvfile:
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
-
+   
     count = 0
     sum = 0
-    # Read each row of data after the header
+    profits_losses = []
+    first_row = (next(csvreader))
+    prev_num = int(first_row[1])
     for row in csvreader:
-        # print(row)
         count= count + 1
-        # print(count)
         sum = sum + int(row[1])
+        profits_losses.append(prev_num - int(row[1]))
+        prev_num = int(row[1])
+print(profits_losses)
+
+    
+ 
+sum_num = 0
+for x in range(len(profits_losses)-1):
+    sum_num = sum_num + profits_losses[x]           
+
+avg = sum_num / len(profits_losses)
+ 
+
+ # Your task is to create a Python script that analyzes the records to calculate each of the following:
 print("Financial Analysis")
 print("-----------------------------")
-print(f"Total Months:{count}")
-print(f"Total: ${sum}")
-
-# Your task is to create a Python script that analyzes the records to calculate each of the following:
-
 
 # The total number of months included in the dataset
-
+print(f"Total Months:{count}")
 
 # The net total amount of "Profit/Losses" over the entire period
+print(f"Total: ${sum}")
 
-# Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
-
+# # Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
+print(avg)
 
 # The greatest increase in profits (date and amount) over the entire period
-
+print(max(profits_losses))
 
 # The greatest decrease in losses (date and amount) over the entire period
+print(min(profits_losses))
